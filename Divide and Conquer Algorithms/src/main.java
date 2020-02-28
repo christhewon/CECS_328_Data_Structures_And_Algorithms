504057660184d70aebdbe5adc31d755b630566c1
Christopher Lee <ChrisLee@ChristohersMBP2.attlocal.net>
2020-02-28 12:29:34 -0800

    Updated the main
    Allows user to enter input

--- Divide and Conquer Algorithms/src/main.java
+++ Divide and Conquer Algorithms/src/main.java
@@ -1,4 +1,12 @@
 import java.util.Scanner;
 
 public class main {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		int input;
+		System.out.println("Please enter an integer");
+		input = scan.nextInt();
+		
+		
+	}
 }

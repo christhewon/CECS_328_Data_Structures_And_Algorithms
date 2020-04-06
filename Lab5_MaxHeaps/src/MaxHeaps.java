0f62fce6f37819cd44afb5252fac179b36d3fb72
Christopher Lee <chrislee752@yahoo.com>
2020-04-05 19:29:13 -0700

    Swap method and Javadoc
    Added a swap method to clean up code and added Javadoc

--- Lab5_MaxHeaps/src/MaxHeaps.java
+++ Lab5_MaxHeaps/src/MaxHeaps.java
@@ -1,6 +1,6 @@
 public class MaxHeaps {
 	/**
-	 *
+	 * takes an array and creates a maxHeap which is then sorted
 	 * @param arr the array getting heap sorted
 	 * @param n the number of indexes of the array to be sorted from 0 to n
 	 */
@@ -10,10 +10,7 @@ public class MaxHeaps {
 		int temp;
 		buildMaxHeap(arr, n);
 		for(int i = n - 1; i >= 0; i--) { //MIGHT BE INDEX OUT OF BOUNDS
-			temp = arr[0];
-			arr[0] = arr[i];
-			arr[i] = temp;
-			//swap(arr, 0, i);
+			swap(arr, 0, i);
 			flag++;
 			maxHeapify(arr, 0, flag, n);   // flag is one of the parameters because we want to make sure the children are
 										// still "in" the array
@@ -22,7 +19,7 @@ public class MaxHeaps {
 	}
 	
 	/**
-	 *
+	 *  takes an array and builds a max heap using max heapify
 	 * @param arr the array that is getting built into a max heap
 	 * @param n the number of indexes of the array to be built into a max heap from 0 to n
 	 */
@@ -33,7 +30,7 @@ public class MaxHeaps {
 	}
 	
 	/**
-	 *
+	 * takes an array and compares the parent node to its children and sees if a swap is necessary to build a max tree
 	 * @param arr The array that is being max heapified
 	 * @param i The node or index at which we are going to max heapify
 	 * @param x The marker used to "delete" the ends of the array after
@@ -51,24 +48,21 @@ public class MaxHeaps {
 		if(right < length - x && arr[right] > arr[max]) { // checking if a left child exist and if it is greater than
 			max = right;					          // the parent
 		}
-		if(max != i) { //making sure the parent wasn't greater than the children
-			temp = arr[max];
-			arr[max] = arr[i];
-			arr[i] = temp;
-			//swap(arr, max, i);
+		if(max != i) { //Only if parent was less than the children
+			swap(arr, max, i);
 			maxHeapify(arr, max, x, n);
 		}
 	}
 	
 	/**
-	 *
-	 * @param arr
-	 * @param a
-	 * @param b
+	 * takes elements of an array and swaps them
+	 * @param arr the array in which the elements are being swapped
+	 * @param a the first element being swapped
+	 * @param b the second element being swapped
 	 */
 	public static void swap(int[] arr, int a, int b) {
 		int temp = arr[a];
-		arr[a] = b;
+		arr[a] = arr[b];
 		arr[b] = temp;
 	}
 }

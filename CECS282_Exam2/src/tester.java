933c714cb36a7d01024dfe6ab8c11b3fa76e4f5b
Christopher Lee <chrislee752@yahoo.com>
2020-04-20 09:48:12 -0700

    tester creations
    created a tester

---
+++ CECS282_Exam2/src/tester.java
@@ -0,0 +1,31 @@
+public class tester {
+	public static void main(String[] args) {
+		
+		//Tester for prob 1
+		int[] arr = new int[]{5,6,34,78,99,2};
+		int ans = Methods.findMins2(arr);
+		System.out.println(ans);
+		
+		
+		
+		/*
+		//Tester for prob 2
+		int[][] matrix = { {1,5,8,20},
+		                   {2,7,23,28},
+		                   {5,8,12,22} };
+		int[] arr = Methods.convergeMatrixSort(matrix);
+		int length = arr.length;
+		System.out.println();
+		
+		//Methods.heapSort(arr, length);
+		System.out.println("After sorted");
+		for(int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i] + ", ");
+		}
+		
+		*/
+		
+		
+		
+	}
+}

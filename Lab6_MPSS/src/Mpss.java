ae673c052c93af3cd92ccdb7f074c60eb1ae94b9
Christopher Lee <chrislee752@yahoo.com>
2020-04-25 10:03:30 -0700

    Javadoc and Commenting
    Added Javadoc and some comments

--- Lab6_MPSS/src/Mpss.java
+++ Lab6_MPSS/src/Mpss.java
@@ -1,7 +1,14 @@
 import java.util.Arrays;
 
 public class Mpss {
-	public static int minimumPositiveSubSequence(int[] arr, int l, int r) {
+	/**
+	 * takes an array and find the minimum positive subsequent sum
+	 * @param arr the array being used to find the sum
+	 * @param l the left bound index of the array
+	 * @param r the right bound index of the array
+	 * @return the minimum positive subsequent sum
+	 */
+	public static int minimumPositiveSubsequenceSum(int[] arr, int l, int r) {
 		int mpssl;
 		int mpssr;
 		int mpssm;
@@ -16,32 +23,22 @@ public class Mpss {
 			}
 		}
 		else {
-			mpssl = minimumPositiveSubSequence(arr, l, mid);
-			mpssr = minimumPositiveSubSequence(arr, mid + 1, r);
-			mpssm = findMPSS(arr, l, r);
+			mpssl = minimumPositiveSubsequenceSum(arr, l, mid);
+			mpssr = minimumPositiveSubsequenceSum(arr, mid + 1, r);
+			mpssm = findMPSSm(arr, l, r);
 			min = Math.min(mpssl,mpssr);
 			return Math.min(min, mpssm);
 		}
-		
-					/*
-		System.out.println("left: ");
-		for(int i = mid; i >= 0; i--) {
-			System.out.print(sL[i]);
-		}
-		System.out.println();
-		System.out.print("right: ");
-		for(int i = mid; i >= 0; i--) {
-			System.out.print(sR[i]);
-		}
-	
-		*/
-		
-		
-
-		
 	}
 	
-	public static int findMPSS(int[] arr, int l, int r) {
+	/**
+	 * Finds the minimum positive subsequent sum that may cross the left and right half of the array
+	 * @param arr the array being used to find the sum
+	 * @param l the left bound index of the array
+	 * @param r the right bound index of the array
+	 * @return the minimum positive subsequent sum of the middle(crossing the left or right half of the array)
+	 */
+	public static int findMPSSm(int[] arr, int l, int r) {
 		int mid = (l + r)/2;
 		int[] sL = new int[mid + 1];
 		int[] sR = new int[mid + 1];
@@ -62,9 +59,6 @@ public class Mpss {
 			sR[i - mid - 1] = rightSum;
 		}
 		
-		
-		//quickSort(sL);
-		//quickSortDescend(sR);
 		Arrays.sort(sL);
 		Arrays.sort(sR);
 		
@@ -80,6 +74,7 @@ public class Mpss {
 		}
 		
 		//TEST
+		/*
 		System.out.println("This is the left arr");
 		for(int i = 0; i < sL.length; i++) {
 			System.out.print(sL[i] + ", ");
@@ -90,6 +85,7 @@ public class Mpss {
 		for(int i = 0; i < sR.length; i++) {
 			System.out.print(sR[i] + ", ");
 		}
+		*/
 		
 		int i = 0;
 		int j = 0;
@@ -112,67 +108,6 @@ public class Mpss {
 		return ans;
 	}
 	
-	
-	public static void quickSort(int[] arr, int start, int end) {
-	
-	}
-	
-	public static void quickSortDescend(int[] arr, int start, int end) {
-	
-	}
-	
-	private int partitions(int[] arr, int start, int end) {
-		//CORRECT
-		int pivot = arr[end];
-		int length = arr.length;
-		int j = 0;      //This is gonna be the location that switched with the pivot
-		int k = end - 1;
-		int temp = 0;
-		while (j < k) { // change to while loop to until j and k cross
-			while (arr[j] < pivot && j < length) { //basically if the number is smaller than the pivot, we dont car and move to next index change to while
-				j++;
-			}
-			
-			while (arr[k] > pivot && k > 0) {              //Now that we have a number greater than the pivot, we want to find a
-				k--;
-			}
-			if (j < k) {      //number on the other side of the array that is smaller than the pivot
-				temp = arr[k];         //and swap them
-				arr[k] = arr[j];
-				arr[j] = temp;
-			}
-			
-		}
-		temp = arr[end];                //All the values to the left of j are less than the pivot, so the pivot goes
-		arr[end] = arr[j];               //at j
-		arr[j] = temp;
-		return j;
-	
-		/*
-	public static int[] quickSor(int[] arr) {
-		int l = 0;
-		int r = arr.length - 1;
-		int piv = arr.length - 1;
-		int temper = 0;
-		while(true) {
-			if(arr[l] <= piv) {
-				l++;
-			}
-			else {
-				while(arr[r] >= piv) {
-					r++;
-				}
-				temper = arr[l];
-				arr[l] = arr[r];
-				arr[r] = temper;
-			}
-			if(l > r) {
-				return arr;
-			}
-		}
-		
-		 */
-	}
 
 	
 }

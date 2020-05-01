cccabc2b15df8d62414b4cc17ca56ba21e054f07
Christopher Lee <chrislee752@yahoo.com>
2020-05-01 14:38:14 -0700

    Created Tester

---
+++ Lab8_Depth_First_Search/src/tester.java
@@ -0,0 +1,10 @@
+import org.w3c.dom.ls.LSOutput;
+
+public class tester {
+	public static void main(String[] args) {
+		int timer = 0;
+		System.out.println(timer++);
+		System.out.println(timer++);
+	}
+	
+}

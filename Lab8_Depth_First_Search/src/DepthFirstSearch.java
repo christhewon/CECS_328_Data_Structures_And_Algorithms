7d912eb94ce4adfff305b9b5467ab40e0f9f3881
Christopher Lee <chrislee752@yahoo.com>
2020-05-07 15:39:22 -0700

    Created Depth First Search Class
    The class contains the DFS algorithm

--- Lab8_Depth_First_Search/src/DepthFirstSearch.java
+++ Lab8_Depth_First_Search/src/DepthFirstSearch.java
@@ -1,3 +1,46 @@
-public class DepthFirstSearch {
+import java.util.LinkedList;
 
+public class DepthFirstSearch {
+	private static int timer = 1;
+	private static boolean cycle = false;
+	
+	public static LinkedList<Node> DFS(Node[] list) {
+		Node cur;
+		Node start = new Node("start");
+		start.setStart(0);
+		start.setEnd(0);
+		LinkedList<Node> topology = new LinkedList<Node>();
+		for(int i = 0; i < list.length; i++) {
+			cur = list[i];
+			if(cur.getParent() == null) {
+				cur.setParent(start);
+				DFSVisit(cur, topology);
+			}
+		}
+		return topology;
+	}
+	
+	public static void DFSVisit(Node n, LinkedList<Node> link) {
+		n.setStart(timer++);
+		LinkedList<Node> list = n.getAdjList();
+		for(int i = 0; i < list.size(); i++) {
+			Node cur = list.get(i);
+			if(cur.getStart() == -1) {
+				cur.setParent(n);
+				DFSVisit(cur, link);
+			}
+			else if(cur.getEnd() == -1) {
+				System.out.println();
+				System.out.println(cur.getName() + " " + n.getName() + " cause a cycle");
+				cycle = true;
+			}
+		}
+		n.setEnd(timer++);
+		link.add(n);
+	}
+	
+	public static boolean getCycle() {
+		return cycle;
+	}
 }
+
